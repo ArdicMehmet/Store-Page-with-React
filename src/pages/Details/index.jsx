e76c48be5da73380d103b150ce7558d7f9eadefe
Mehmet Ardic <ardicmehmet96@gmail.com>
2024-06-18 11:28:44 +0300

    changed button name

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -60,7 +60,7 @@ function Details() {
                 className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 mt-6"
                 onClick={addToBasket}
               >
-                Sepete Ekle
+                Add to basket
               </button>
             </figcaption>
           </div>
